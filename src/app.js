85b037eb68d1a80d2225816157423409ca862444
Shubham Dudhe <dudheshubham56@gmail.com>
2024-12-08 02:08:37 +0530

    adding route on app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,66 @@
 const express=require("express");
-
+  
 const app=express();
+
+// Middleware to parse JSON body
+app.use(express.json());
 
 //reques handler
 
-app.use("/test/1",(req,res)=>{
-    res.send({'status':'success','msg':"successfully get msg id which 4"});
+// basic route
+app.get("/product",(req,res)=>{
+    res.send("list of product availble here");
 })
-//manage routs in express::
-app.use("/test",(req,res)=>{
-    res.send({'status':'success','msg':"successfully listening"});
+
+app.get("/getuser",(req,res)=>{
+    res.send("list of users::");
+})
+
+//route with parameters::
+
+app.get("/users/:id",(req,res)=>{
+    //get id or other thing from url use req.params
+    let user_id=req.params.id;
+    res.send(`here is user id ${user_id}`)
+})
+app.get("/users/:id/:book_id",(req,res)=>{
+    //get id or other thing from url use req.params
+    let user_id=req.params.id;
+    let book_id=req.params.book_id;
+    res.send(`here is user id ${user_id} and book id is ${book_id}`)
+})
+
+//query parameters:
+//query parameter is pass after url with ? symbol ::
+app.get('/queryparameter',(req,res)=>{
+    console.log(req.query);
+    const {first,second}=req.query;
+    res.send(`first val is ${first} and second one is ${second}`);
+})
+
+app.get('/login', (req, res) => { // Access data from the body
+    res.send(`login successfully ::`);
+  });
+
+
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body; // Access data from the body
+    res.send(`Username: ${username}, Password: ${password}`);
+  });
+
+
+
+
+
+app.post("product?user_id=1",(req,res)=>{
+    console.log(req.params);
+    console.log("geting user id");
+    
 })
 
 
 app.use("/",(req,res)=>{
-    res.send("base url functionality::");
+   return  res.send("base url functionality::");
 })
 
 
